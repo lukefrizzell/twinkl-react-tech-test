aaead0d670239e517a440efacd4ee79b6b4910e6
Luke Frizzell <lukefrizzell97@gmail.com>
2024-07-24 19:28:30 +0100

    add remove button

--- src/components/Post/Post.test.tsx
+++ src/components/Post/Post.test.tsx
@@ -5,15 +5,21 @@ import { PostContent } from "../../PostContent";
 const POST: PostContent = { id: 1, title: "Post 1", body: "Post 1 body" };
 
 describe("<Post />", () => {
-    it("displays the post title", () => {
-        render(<Post post={POST} />);
-        const postTitle = screen.getByText(POST.title);
-        expect(postTitle).toBeInTheDocument();
-    });
+  it("displays the post title", () => {
+    render(<Post post={POST} />);
+    const postTitle = screen.getByText(POST.title);
+    expect(postTitle).toBeInTheDocument();
+  });
 
-    it("displays the post body", () => {
-        render(<Post post={POST} />);
-        const postTitle = screen.getByText(POST.body);
-        expect(postTitle).toBeInTheDocument();
-    });
-})
+  it("displays the post body", () => {
+    render(<Post post={POST} />);
+    const postTitle = screen.getByText(POST.body);
+    expect(postTitle).toBeInTheDocument();
+  });
+
+  it("displays a remove button", () => {
+    render(<Post post={POST} />);
+    const removeButton = screen.getByRole("button", { name: "Remove" });
+    expect(removeButton).toBeInTheDocument();
+  });
+});
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import './Post.css'
+import "./Post.css";
 import { PostContent } from "../../PostContent";
 
 export const Post: React.FC<{ post: PostContent }> = ({ post }) => {
   return (
     <div className="post">
-      <div>{post.title}</div>
-      <div>{post.body}</div>
+      <div className="post-content">
+        <div>{post.title}</div>
+        <div>{post.body}</div>
+      </div>
+      <div className="post-actions"><button onClick={() => {}}>Remove</button></div>
     </div>
   );
 };
