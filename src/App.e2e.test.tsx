5fbde8235f99cb8adecd0f5387000bcacbc86c3e
Luke Frizzell <lukefrizzell97@gmail.com>
2024-07-24 17:42:22 +0100

    implement loading state

--- src/App.e2e.test.tsx
+++ src/App.e2e.test.tsx
@@ -2,9 +2,15 @@ import {render, screen} from '@testing-library/react';
 import App from './App';
 
 describe('List of Posts', () => {
-    it('should render a list of posts', () => {
+    it('should render a loading state', () => {
         render(<App />);
-        const posts = screen.getAllByRole('listitem');
+        const loading = screen.getByText('Loading...');
+        expect(loading).toBeInTheDocument();
+    });
+
+    it('should render a list of posts', async () => {
+        render(<App />);
+        const posts = await screen.findAllByRole('listitem');
         expect(posts.length).toBeGreaterThan(0);
     });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import "./App.css";
 import { usePosts } from "./usePosts";
 
 const App = () => {
-  const { posts } = usePosts();
+  const { posts, loading } = usePosts();
 
   return (
     <div className="container">
       <div className="container-inner">
+        {loading && <div>Loading...</div>}
         <Posts posts={posts} />
       </div>
     </div>
